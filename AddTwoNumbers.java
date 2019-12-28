f82e7108b227d59ae67209c1e547cabae96b6fb8
saivardhan31217 <59111400+saivardhan31217@users.noreply.github.com>
2019-12-28 14:22:36 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,10 @@
+public class AddTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int a = 5, b = 15, c;
+      c = a + b;
+
+      System.out.println("Sum of these numbers: "+c);
+   }
+}
